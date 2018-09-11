f1b609b11feebc1e633b070a9efb8526f0284ba0
axsoray <axsoray@gmail.com>
2018-09-11 18:30:07 +0300

    Add service reference.

--- WCFDemo/WCFDemo/ViewModels/MainPageViewModel.cs
+++ WCFDemo/WCFDemo/ViewModels/MainPageViewModel.cs
@@ -1,19 +1,47 @@
-﻿using Prism.Commands;
-using Prism.Mvvm;
+﻿using System;
+using Prism.Commands;
 using Prism.Navigation;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using TempConverter;
+
 
 namespace WCFDemo.ViewModels
 {
     public class MainPageViewModel : ViewModelBase
     {
-        public MainPageViewModel(INavigationService navigationService) 
-            : base (navigationService)
+        private string _input;
+        public string Input
         {
-            Title = "Main Page";
+            get { return _input; }
+            set { SetProperty(ref _input, value); }
+        }
+
+        private string _result;
+        public string Result
+        {
+            get { return _result; }
+            set { SetProperty(ref _result, value); }
+        }
+
+        public DelegateCommand Calculate { get; set; }
+
+        public MainPageViewModel(INavigationService navigationService)
+            : base(navigationService)
+        {
+            Title = "WCF Demo";
+
+            Calculate = new DelegateCommand(CalculateExecute);
+        }
+
+        private async void CalculateExecute()
+        {
+            try {
+                var client = new TempConvertSoapClient(TempConvertSoapClient.EndpointConfiguration.TempConvertSoap12);
+                string celsius = await client.FahrenheitToCelsiusAsync(Input);
+
+                Result = $"The temp in celsius is: {celsius}";
+            } catch (Exception ex) {
+                Result = $"Error: {ex.Message}";
+            }
         }
     }
 }
